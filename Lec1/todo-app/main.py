58925dcdb07d11c1b11cb3f0fbaafb5475499a82
DevOps <devops.shastra@gmail.com>
2025-12-13 00:19:24 +0530

    Update main.py

--- Lec1/todo-app/main.py
+++ Lec1/todo-app/main.py
@@ -1,5 +1,19 @@
-message = "Enter a todo:"
+# Welcome Message Section
 print("-" * 100)
-print(" " * 40, "Welcome to Todo Apps")
+print(" " * 35, "Welcome to Todo Application")
 print("-" * 100)
-print(message)
+
+
+# User Input Section
+user_prompt = "Enter a todo: "
+
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+
+
+print(todos)
+print("Total todo:", len(todos))
+print(type(todos))
