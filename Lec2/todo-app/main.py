379a26dbf1e72be336417b04ab4e9c33363323a1
noob-programmer89 <mayur.kalas@gmail.com>
2025-12-24 11:54:03 +0530

    Lec2 Added

---
+++ Lec2/todo-app/main.py
@@ -0,0 +1,14 @@
+print("-" * 100)
+print(" " * 35, "Welcome to Todo Application")
+print("-" * 100)
+
+# User Input Section
+user_prompt = "Enter a todo: "
+
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+
+print(todos)
